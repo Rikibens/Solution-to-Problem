3b7b0b75c52a1f273f7098dacbdab92f714ada33
Vaslcs <vales0797@gmail.com>
2022-07-19 19:52:18 +0800

    intersection 2 arr

---
+++ Intersection of Two Arrays II/Intersection of Two Arrays II/Intersection of Two Arrays II.cpp
@@ -0,0 +1,23 @@
+// Intersection of Two Arrays II.cpp
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+   
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+
+    nums1.erase(set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), nums1.begin()),nums1.end());
+
+
+     return  nums1;
+};
+int main()
+{
+
+   
+}
